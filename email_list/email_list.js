970324c98b3afa9340ce1d37f3cf071621b3f901
TannerWinchester <73656486+TannerWinchester@users.noreply.github.com>
2021-01-27 11:27:29 -0700

    chapter6

--- email_list/email_list.js
+++ email_list/email_list.js
@@ -4,35 +4,44 @@ document.addEventListener("DOMContentLoaded", () => {
     
     $("#join_list").addEventListener("click", () => {
         // get values user entered in textboxes
-        const email1 = $("#email_1").value;
-        const email2 = $("#email_2").value;
-        const firstName = $("#first_name").value;
+        const email1 = $("#email_1");
+        const email2 = $("#email_2");
+        const firstName = $("#first_name");
     
-        // create an error message and set it to an empty string
-        let errorMessage = "";
+        // boolean variable for invalid entries
+        let isValid = true;
 
         // check user entries - add text to error message if invalid
-        if (email1 == "") { 
-            errorMessage += "First email is required.\n";
-        }
-    
-        if (email2 == "") { 
-            errorMessage += "Second email is required.\n";
-        }
-    
-        if (email1 != email2) { 
-            errorMessage += "Both emails must match.\n";
-        }
-    
-        if (firstName == "") {
-            errorMessage += "First name is required.\n";
-        }
-    
-        // submit the form if error message is an empty string
-        if (errorMessage == "") {
-            $("#email_form").submit();
+        if (email1.value == "") {
+            email1.nextElementSibling.textContent = "This field is required.";
+            isValid = false;
         } else {
-            alert(errorMessage);            
+            email1.nextElementSibling.textContent = "";
+        }
+
+        if (email2.value == "") {
+            email2.nextElementSibling.textContent = "This field is required."
+            isValid = false;
+        } else {
+            email2.nextElementSibling.textContent = "";
+        }
+
+        if (email1.value != email2.value) {
+            email2.nextElementSibling.textContent = "Email addresses must match.";
+            isValid = false;
+        }
+
+        if (firstName.value == "") {
+            firstName.nextElementSibling.textContent = "First name is required."
+            isValid = false;
+        } else {
+            firstName.nextElementSibling.textContent = "";
+        }
+
+        //submit if all entries are valid.
+
+        if (isValid) {
+            $("#email_form").submit();
         }
     });
 
