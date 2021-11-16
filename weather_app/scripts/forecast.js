f818524343deb572574a08b854fc07a038858b27
TannerWinchester <73656486+TannerWinchester@users.noreply.github.com>
2021-11-15 17:49:01 -0700

    JavaScript weather app

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,28 @@
+// weather API website https://developer.accuweather.com/
+const key = 'qDz2K9JidlAshZREKPtP27hizkVAkMSD';
+
+// get weather information
+const getWeather = async id => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
